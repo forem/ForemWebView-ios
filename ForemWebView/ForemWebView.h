196bf89a32a64ab69bdfedf409ac4386eedb6b49
Fernando Valverde <fdov88@gmail.com>
2020-09-28 16:39:09 -0600

    Initial Commit

---
+++ ForemWebView/ForemWebView.h
@@ -0,0 +1,18 @@
+//
+//  ForemWebView.h
+//  ForemWebView
+//
+//  Created by Fernando Valverde on 9/28/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ForemWebView.
+FOUNDATION_EXPORT double ForemWebViewVersionNumber;
+
+//! Project version string for ForemWebView.
+FOUNDATION_EXPORT const unsigned char ForemWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ForemWebView/PublicHeader.h>
+
+
