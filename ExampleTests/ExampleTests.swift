6eb1e2b77b45e5ab43662b92a2dd3d685aafdc9c
Fernando Valverde <fdov88@gmail.com>
2020-10-29 08:54:43 -0600

    More fogiving timeouts in tests for CI

--- ExampleTests/ExampleTests.swift
+++ ExampleTests/ExampleTests.swift
@@ -5,8 +5,12 @@ import WebKit
 class ExampleTests: XCTestCase {
     
     var viewController: ViewController!
+    
+    // These values need to be tuned for CI performance. Keep in mind that locally you may be
+    // running on better specs than CI will be using to run these, that's the reason behind them
+    // being large (forgiving). It would be good to find a way to avoid having to do the async dispatches
     let asyncAfter = 4.0
-    let timeout = 7.0
+    let timeout = 10.0
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
