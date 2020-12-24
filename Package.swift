da1e73eeba3c2dd577c97b004a945cf0d74bb74d
Fernando Valverde <fdov88@gmail.com>
2020-12-24 07:28:32 -0600

    Package.swift testing

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swiftlint:disable all
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// swiftlint:disable all
 
 import PackageDescription
 
 let package = Package(
-    name: "ForemWebView-ios",
+    name: "ForemWebView",
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Yummypets/YPImagePicker", branch: "spm"),
+        .package(url: "https://github.com/Yummypets/YPImagePicker", .branch("spm")),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.1.0"),
     ],
@@ -23,9 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ForemWebView",
-            dependencies: []),
+            dependencies: ["YPImagePicker", "Alamofire", "AlamofireImage"],
+            path: "Sources"),
         .testTarget(
             name: "ForemWebViewTests",
-            dependencies: ["ForemWebView"]),
+            dependencies: ["ForemWebView"],
+            path: "Tests"),
     ]
 )
