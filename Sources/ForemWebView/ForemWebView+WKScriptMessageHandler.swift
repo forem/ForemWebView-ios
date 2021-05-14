82928dea5c09f51ef9da2422060a3b16301da47b
Fernando Valverde <fernando@visualcosita.com>
2021-05-14 16:02:41 -0600

    debugging

--- Sources/ForemWebView/ForemWebView+WKScriptMessageHandler.swift
+++ Sources/ForemWebView/ForemWebView+WKScriptMessageHandler.swift
@@ -120,11 +120,15 @@ extension ForemWebView: WKScriptMessageHandler {
                          console.log("Injecting message")
                          var element = document.getElementById("\(targetElementId)");
                          console.log("Target element: ", element)
-                         element.value = `\(jsonString)`;
-                         console.log("Target element (again): ", element)
-                         var changeEvent = new Event('input', { bubbles: true });
-                         element.dispatchEvent(changeEvent);
+                         
                          """
+        
+//        element.value = `\(jsonString)`;
+//        console.log("Target element (again): ", element)
+//        var changeEvent = new Event('input', { bubbles: true });
+//        element.dispatchEvent(changeEvent);
+        
+        javascript.replacingOccurrences(of: "\\\"", with: "")
         evaluateJavaScript(javascript) { res, error in
             print("WELP: \(String(describing: res)) - \(String(describing: error))")
             guard error == nil else {
