4b822610a96d6bf2cd3cd27c2fef08395754a831
Fernando Valverde <fernando@visualcosita.com>
2021-05-14 15:53:49 -0600

    Replace single with double quotes

--- Sources/ForemWebView/ForemWebView+WKScriptMessageHandler.swift
+++ Sources/ForemWebView/ForemWebView+WKScriptMessageHandler.swift
@@ -118,7 +118,7 @@ extension ForemWebView: WKScriptMessageHandler {
         // programmatically, so we are forced to dispatch the event manually
         let javascript = """
                          console.log("Injecting message")
-                         var element = document.getElementById('\(targetElementId)');
+                         var element = document.getElementById("\(targetElementId)");
                          console.log("Target element: ", element)
                          element.value = `\(jsonString)`;
                          console.log("Target element (again): ", element)
