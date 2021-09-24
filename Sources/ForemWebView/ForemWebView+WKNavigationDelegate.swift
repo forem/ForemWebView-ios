42667bd190c5cb0fafd8e197326365d83fddaeed
Fernando Valverde <fernando@visualcosita.com>
2021-09-24 10:15:15 -0600

    Fix target="_blank" links (#39)
    * Open windows automatically
    
    * Another test
    
    * log for test
    
    * Handle target blanks
    
    * Reverse guard clause
    
    * Cleanup

--- Sources/ForemWebView/ForemWebView+WKNavigationDelegate.swift
+++ Sources/ForemWebView/ForemWebView+WKNavigationDelegate.swift
@@ -42,6 +42,14 @@ extension ForemWebView: WKNavigationDelegate {
             decisionHandler(.allow)
             return
         }
+        guard navigationAction.targetFrame != nil else {
+            // target="_blank" normal navigation won't work (returning .allow policy)
+            // In order for the webview to follow links (even within an iframe) requires
+            // us to capture the navigation and "cancel it", but then manually loading the URL
+            decisionHandler(.cancel)
+            load(url.absoluteString)
+            return
+        }
         let policy = navigationPolicy(url: url, navigationType: navigationAction.navigationType)
         decisionHandler(policy)
     }
