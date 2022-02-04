764514ab021b92265aca8cd1e88b6f9d3a484ea7
Josh Puetz <hi@joshpuetz.com>
2022-02-04 16:18:30 -0600

    Refactor and add tests

--- Tests/ForemWebViewTests/XCTestManifests.swift
+++ Tests/ForemWebViewTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(ForemWebViewTests.allTests),
+        testCase(URL_ForemUtilitiesTests.allTests),
     ]
 }
 #endif
