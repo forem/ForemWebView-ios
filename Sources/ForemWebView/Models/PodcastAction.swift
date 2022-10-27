3a89c02c94a22673d7ee26f24d201edb2a84b03e
Daniel Chick <daniel.chick@hotmail.com>
2022-10-26 21:17:46 -0500

    Add Podcast poso

---
+++ Sources/ForemWebView/Models/PodcastAction.swift
@@ -0,0 +1,30 @@
+//
+//  PodcastAction.swift
+//  
+//
+//  Created by Daniel Chick on 10/26/22.
+//
+
+import Foundation
+
+public final class Podcast: Codable {
+    let action: PodcastAction
+    let url: String?
+    let seconds: String?
+    let rate: Float?
+    let muted: Bool?
+    let volume: Float?
+}
+
+public enum PodcastAction: String, Codable {
+    case play
+    case load
+    case seek
+    case rate
+    case muted
+    case pause
+    case terminate
+    case volume
+    case metadata
+    
+}
